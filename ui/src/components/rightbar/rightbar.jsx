c6a092e7f2e2dc114930ebb0a9751333f96fed59
Wenxin Yang <yang.wenxin@northeastern.edu>
2021-07-24 20:31:54 +0800

    ui finished static page

--- ui/src/components/rightbar/rightbar.jsx
+++ ui/src/components/rightbar/rightbar.jsx
@@ -4,7 +4,29 @@ import "./rightbar.css"
 function Rightbar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg"/>
+                    <span className="birthdayText">
+                        {" "}
+                        <b>Alophose</b> and <b>3 other friends</b> have a birthday today!
+                    </span>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAd"/>
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarImgContainer">
+                            <img
+                                src="assets/person/3.jpeg" alt=""
+                                className="rightbarProfileImg"/>
+
+                            <span className="rightbarOnline"> </span>
+                        </div>
+                        <span className="rightbarUserName">Wendy Carter</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
