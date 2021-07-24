8739285c99900858066d37d6f1d878685a423671
Wenxin Yang <yang.wenxin@northeastern.edu>
2021-07-24 19:00:39 +0800

    feed

--- ui/src/components/share/share.jsx
+++ ui/src/components/share/share.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./share.css";
-import { PermMedia } from "@material-ui/icons"
+import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons"
 
 function Share() {
     return (
@@ -10,14 +10,26 @@ function Share() {
                     <img src="/assets/person/1.jpeg" className="shareProfileImg"/>
                     <input
                         placeholder="what's in your mind?"
-                        className="shareIput"/>
+                        className="shareInput"/>
                 </div>
                 <hr className="shareHr"/>
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
-                            <PermMedia className="shareIcon" />
+                            <PermMedia htmlColor="tomato" className="shareIcon" />
                             <span className="shareOptionText">Photo or Video</span>
+                        </div>
+                        <div className="shareOption">
+                            <Label htmlColor="blue" className="shareIcon" />
+                            <span className="shareOptionText">Tag</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor="green" className="shareIcon" />
+                            <span className="shareOptionText">Location</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+                            <span className="shareOptionText">Feelings</span>
                         </div>
                     </div>
                 </div>
